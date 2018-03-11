64d1661785a0dbc5b68b95b1d5260e8f91568de7
uael <lucas.abel@yandex.com>
2018-03-11 21:04:05 +0100

    some norme

--- src/lib/wctomb.c
+++ src/lib/wctomb.c
@@ -13,41 +13,22 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#define IS_CODEUNIT(c) ((unsigned)(c) - 0xdf80 < 0x80)
-
-int	ft_wctomb(char *s, wchar_t wc)
+static int	wctomb2(char *s, wchar_t wc)
 {
-	if (!s)
-		return (1);
-	if ((unsigned)wc < 0x80)
-	{
-		*s = (char)wc;
-		return (1);
-	}
-	else if (MB_CUR_MAX == 1)
-	{
-		if (!IS_CODEUNIT(wc))
-		{
-			errno = EILSEQ;
-			return (-1);
-		}
-		*s = (char)wc;
-		return (1);
-	}
-	else if ((unsigned)wc < 0x800)
+	if ((unsigned)wc < 0x800)
 	{
 		*s++ = (char)(0xc0 | (wc >> 6));
 		*s = (char)(0x80 | (wc & 0x3f));
 		return (2);
 	}
-	else if ((unsigned)wc < 0xd800 || (unsigned)wc - 0xe000 < 0x2000)
+	if ((unsigned)wc < 0xd800 || (unsigned)wc - 0xe000 < 0x2000)
 	{
 		*s++ = (char)(0xe0 | (wc >> 12));
 		*s++ = (char)(0x80 | ((wc >> 6) & 0x3f));
 		*s = (char)(0x80 | (wc & 0x3f));
 		return (3);
 	}
-	else if ((unsigned)wc - 0x10000 < 0x100000)
+	if ((unsigned)wc - 0x10000 < 0x100000)
 	{
 		*s++ = (char)(0xf0 | (wc >> 18));
 		*s++ = (char)(0x80 | ((wc >> 12) & 0x3f));
@@ -57,4 +38,26 @@ int	ft_wctomb(char *s, wchar_t wc)
 	}
 	errno = EILSEQ;
 	return (-1);
+}
+
+int			ft_wctomb(char *s, wchar_t wc)
+{
+	if (!s)
+		return (1);
+	if ((unsigned)wc < 0x80)
+	{
+		*s = (char)wc;
+		return (1);
+	}
+	if (MB_CUR_MAX == 1)
+	{
+		if ((unsigned)wc - 0xdf80 >= 0x80)
+		{
+			errno = EILSEQ;
+			return (-1);
+		}
+		*s = (char)wc;
+		return (1);
+	}
+	return (wctomb2(s, wc));
 }
